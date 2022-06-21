590ff05f626fb06639cb02c91559c11b991bdce8
loyalgametr <sed3718@gmail.com>
2022-06-21 10:50:16 +0300

    Addition of some comment lines

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,22 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
+        // assign variable to keep values
         int e1 = 1;
         int e2 = 1;
         int e3 = 0;
 
+        // a variable to create and order of approach
         int countTheOrder = 0;
+        // get input for to see which element the user want to see
         System.out.println("Which element do you want to see?: ");
         int last_element = scan.nextInt();
 
+        // iteration for finding the last element of fibonacci series
+        // start of iteration
         for (int i = 0;i<last_element;i++ ){
-
+            // from now on it will be in an order like:
+            // e1 e2 e3 e1 e2 e3 e1 e2 e3 ...
             if (countTheOrder%3 == 0){
                 e3 = e1 + e2;
             }
@@ -26,8 +31,10 @@ public class Main {
             }
 
             countTheOrder++;
-        }
+        }// end of iteration
 
+        // display the desired element of fibonacci using order approach variable
+        // determine which is the last element
         if (countTheOrder%3 == 0){
             System.out.println("The "+last_element + " th number is " + e3);
         }
