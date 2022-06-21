e76ad4927cfe6c30928c7ea7f0bf29512a97ca7d
loyalgametr <70113249+loyalgametr@users.noreply.github.com>
2022-06-21 11:36:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,47 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int a = 1;
+        int b = 1;
+
         Scanner scan = new Scanner(System.in);
-        // assign variable to keep values
-        int e1 = 1;
-        int e2 = 1;
-        int e3 = 0;
+        // get the number of element
+        System.out.println("Which element do you want to see? :");
+        int lastElement = scan.nextInt();
+        // define a variable to keep indexes
+        int count = 1;
 
-        // a variable to create and order of approach
-        int countTheOrder = 0;
-        // get input for to see which element the user want to see
-        System.out.println("Which element do you want to see?: ");
-        int last_element = scan.nextInt();
-
-        // iteration for finding the last element of fibonacci series
-        // start of iteration
-        for (int i = 0;i<last_element;i++ ){
-            // from now on it will be in an order like:
-            // e1 e2 e3 e1 e2 e3 e1 e2 e3 ...
-            if (countTheOrder%3 == 0){
-                e3 = e1 + e2;
-            }
-            else if (countTheOrder %3 == 1){
-                e1 = e2 + e3;
-            }
-            else if (countTheOrder%3 == 2){
-                e2 = e1+e3;
+        while (count < lastElement){
+            // there must be a condition because it will not be correct if there two work simultaneously
+            if (count%2 == 1){
+                a = a + b;
+            } else if (count%2== 0) {
+                b = a + b;
             }
 
-            countTheOrder++;
-        }// end of iteration
+            count++;
+        }
+        // display results as the last item which was found by using condition
+        if (count%2 == 1){
+            System.out.println(a);
+        } else if (count%2 == 0) {
+            System.out.println(b);
+        }
 
-        // display the desired element of fibonacci using order approach variable
-        // determine which is the last element
-        if (countTheOrder%3 == 0){
-            System.out.println("The "+last_element + " th number is " + e3);
-        }
-        else if (countTheOrder%3 == 1){
-            System.out.println("The "+last_element + " th number is " + e1);
-        }
-        else if (countTheOrder%3 == 2){
-            System.out.println("The "+last_element + " th number is " + e2);
-        }
 
     }
 }
